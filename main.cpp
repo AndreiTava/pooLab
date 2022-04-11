90302fb57d1644300abf05561b5a71bc8fd9ba63
Andrei Tava <tavaandrei@gmail.com>
2022-04-11 16:43:18 +0300

    restore

--- main.cpp
+++ main.cpp
@@ -2,12 +2,8 @@
 
 int main()
 {
-    /*Player me("Dumitran");
+    Player me("Dumitran");
     Battle battle(me);
-    battle.play();*/
-    Entity* iWantToDie = new Enemy();
-    std::cout << *iWantToDie << "\n";
-    iWantToDie->takeDamage(10);
-    delete iWantToDie;
+    battle.play();
 }
 
