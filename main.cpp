ebb27e5ebc8961ccdf86801c41b5ebef280ec6cb
Andrei Tava <tavaandrei@gmail.com>
2022-04-11 16:38:54 +0300

    more testing

--- main.cpp
+++ main.cpp
@@ -5,5 +5,8 @@ int main()
     /*Player me("Dumitran");
     Battle battle(me);
     battle.play();*/
+    Entity* iWantToDie = new Enemy();
+    std::cout << *iWantToDie << "\n";
+    delete iWantToDie;
 }
 
